49734eecb80ab72ae80e662e7696245c8bc0afab
njwamustaffa <116859535+njwamustaffa@users.noreply.github.com>
2023-01-06 11:30:25 +0800

    Add files via upload

---
+++ UnifiedNumeral/UnifiedNumeral/PropCons.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnifiedNumeral
+{
+    public partial class UnifiedNumeral
+    {
+        private int _value;
+
+        public int Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+        public UnifiedNumeral()
+        {
+            int _value = 0;
+        }
+    }
+}
