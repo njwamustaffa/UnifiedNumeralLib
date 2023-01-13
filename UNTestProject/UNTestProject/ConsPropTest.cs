dcc83545fb62b98044e4952ac04691905d99e64a
Muhammad Nadzman Nasri <2190768@alfateh.upnm.edu.my>
2023-01-13 12:19:05 +0800

    add test project

---
+++ UNTestProject/UNTestProject/ConsPropTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UnifiedNumeral;
+
+namespace UNTestProject
+{
+    [TestClass]
+    public class ConsPropTest
+    {
+        [TestMethod]
+        public void TestConstructor1()
+        {
+            UnifiedNumeral.UnifiedNumeral a = new UnifiedNumeral.UnifiedNumeral();
+
+            Assert.AreEqual(a.Value, 0);
+        }
+    }
+}
---
+++ UNTestProject/UNTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UNTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UNTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dc94d951-9a58-4c8c-8c72-dd847f44332c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
