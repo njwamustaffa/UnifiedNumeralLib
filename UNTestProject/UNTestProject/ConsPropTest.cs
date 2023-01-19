7eb4bda6d9f31439db2fb32f258bbd8995aa3af6
MUHAMMAD NADZMAN NASRI <2190768@alfateh.upnm.edu.my>
2023-01-20 02:08:15 +0800

    setter getter , add, sub, mul and div test

--- UNTestProject/UNTestProject/ConsPropTest.cs
+++ UNTestProject/UNTestProject/ConsPropTest.cs
@@ -14,5 +14,74 @@ namespace UNTestProject
 
             Assert.AreEqual(a.Value, 0);
         }
+
+        [TestMethod]
+        public void SetterGetterTest()
+        {
+            UnifiedNumeral.UnifiedNumeral a = new UnifiedNumeral.UnifiedNumeral();
+
+            a.Value = 5;
+            Assert.AreEqual(a.Value, 5);
+        }
+
+        [TestMethod]
+        public void AdditionTest()
+        {
+            UnifiedNumeral.UnifiedNumeral a1 = new UnifiedNumeral.UnifiedNumeral();
+            UnifiedNumeral.UnifiedNumeral a2 = new UnifiedNumeral.UnifiedNumeral();
+            UnifiedNumeral.UnifiedNumeral result;
+
+            a1.Value = 5;
+            a2.Value = 10;
+
+            result = a1 + a2;
+
+            Assert.AreEqual(result.Value, 0);
+        }
+
+        [TestMethod]
+        public void SubtractionTest()
+        {
+            UnifiedNumeral.UnifiedNumeral a1 = new UnifiedNumeral.UnifiedNumeral();
+            UnifiedNumeral.UnifiedNumeral a2 = new UnifiedNumeral.UnifiedNumeral();
+            UnifiedNumeral.UnifiedNumeral result;
+
+            a1.Value = 5;
+            a2.Value = 10;
+
+            result = a1 - a2;
+
+            Assert.AreEqual(result.Value, 0);
+        }
+
+        [TestMethod]
+        public void MultiplicationTest()
+        {
+            UnifiedNumeral.UnifiedNumeral a1 = new UnifiedNumeral.UnifiedNumeral();
+            UnifiedNumeral.UnifiedNumeral a2 = new UnifiedNumeral.UnifiedNumeral();
+            UnifiedNumeral.UnifiedNumeral result;
+
+            a1.Value = 5;
+            a2.Value = 10;
+
+            result = a1 * a2;
+
+            Assert.AreEqual(result.Value, 0);
+        }
+
+        [TestMethod]
+        public void DivisonTest()
+        {
+            UnifiedNumeral.UnifiedNumeral a1 = new UnifiedNumeral.UnifiedNumeral();
+            UnifiedNumeral.UnifiedNumeral a2 = new UnifiedNumeral.UnifiedNumeral();
+            UnifiedNumeral.UnifiedNumeral result;
+
+            a1.Value = 5;
+            a2.Value = 10;
+
+            result = a1 / a2;
+
+            Assert.AreEqual(result.Value, 0);
+        }
     }
 }
